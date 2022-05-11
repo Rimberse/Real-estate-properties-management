8bfac00585cd1f103a4d4b33f149f599ff690fa5
Rimberse <kerimpltm@gmail.com>
2022-05-12 00:54:45 +0200

    Implement real-estate property card

--- frontend/src/components/Property.js
+++ frontend/src/components/Property.js
@@ -1,31 +1,44 @@
 import React from 'react';
 
 const Property = ({ property }) => {
-    return (
-        <li className="property">
-          <img src={property.image} alt="Property"></img>
-          <br></br>
-          <b>Adresse :</b> {property.adresse}
-          <br></br>
-          <b>Propriétaire :</b> {property.proprietaire}
-          <br></br>
-          <b>Type:</b> {property.type}
-          <br></br>
-          <b>Nombre de pièces :</b> {property.nbPieces}
-          <br></br>
-          <b>Superficie habitable :</b> {property.superficie}
-          <br></br>
-          <b>Etat d’habitation :</b> {property.etat}
-          <br></br>
-          <b>Prix de mise en vente :</b> {property.prix}
-          <br></br>
-          <b>Date de disponibilité :</b> {property.date}
-          <br></br>
-          <b>Ville :</b> {property.ville}
-          <br></br>
-          <b>Nombre de garages :</b> {property.nbGarages}
-        </li>
-    );
+  // Formats price to the given currency and region. Used to nicely format price of real estates
+  const formatter = new Intl.NumberFormat('fr-FR', {
+    style: 'currency',
+    currency: 'EUR'
+  });
+
+  return (
+    <li className="property">
+      <img src={property.image} alt="Property"></img>
+      <br></br>
+      <div className="property-info">
+        <div className="item">
+          <i className="fa-solid fa-location-dot"></i><span>{property.adresse}</span>
+          <i className="fa-solid fa-city"></i><span>{property.ville}</span>
+        </div>
+        <div className="item">
+          <span><i className={'fa-solid ' + (property.type === 'Maison' ? 'fa-house' : 'fa-building')}></i>{property.type}</span>
+          <span><i className="fa-solid fa-square-full"></i>{property.superficie}</span>
+        </div>
+        <div className="item">
+          <span><i className="fa-solid fa-bed"></i>{property.nbPieces}</span>
+          <span><i className="fa-solid fa-warehouse"></i>{property.nbGarages}</span>
+        </div>
+        <div className="item">
+          <span><i className="fa-solid fa-calendar-days"></i>{property.date.substring(0, property.date.indexOf('T'))}</span>
+          <span><i className="fa-solid fa-clock"></i>{property.date.substring(property.date.indexOf('T') + 1, property.date.indexOf('.'))}</span>
+        </div>
+        <div className="item">
+          <span><i className="fa-solid fa-dollar-sign"></i>{formatter.format(property.prix)}</span>
+          <button className="property-more-btn">Voir</button>
+        </div>
+        <div className="item">
+          <span><i className="fa-solid fa-user"></i>{property.proprietaire}</span>
+          <span><i className="fa-solid fa-circle-check"></i>{property.etat}</span>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default Property;
